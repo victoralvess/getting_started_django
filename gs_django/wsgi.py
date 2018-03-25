99e5be41f63a1d5f870dbf945919c5a7b47e93d7
Victor Alves <vsilva.programador@gmail.com>
2018-03-25 12:45:09 -0300

    Starting Project

---
+++ gs_django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gs_django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gs_django.settings")
+
+application = get_wsgi_application()
