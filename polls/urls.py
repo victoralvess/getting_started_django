c1c86a6a12659174e3370584bfaff0690f9c2633
Victor Alves <vsilva.programador@gmail.com>
2018-03-27 19:10:36 -0300

    Using Django templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
 from . import views
-
+# 'app_name' is used to distinguish different app templates in the project
+app_name = 'polls'
+# The 'name' value will be used by the {% url %} template tag
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('hello/<name>', views.say_hello, name='hello'),
 	path('goodbye/<name>', views.say_goodbye, name='goodbye'),
+	path('<int:question_id>/', views.detail, name='detail'),
+	path('<int:question_id>/results/', views.results, name='results'),
+	path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
