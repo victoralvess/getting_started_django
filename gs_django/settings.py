afa1773c9aebae203bf19ccf5d3cfbeccc5f2410
Victor Alves <vsilva.programador@gmail.com>
2018-03-28 17:51:00 -0300

    Added .env

--- gs_django/settings.py
+++ gs_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '##fh7(5m!bvlvul9u*e8qe=26m^91ja%)xqc16v(gu5ikm_&xa'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
